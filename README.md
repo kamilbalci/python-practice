950f5e3fe0d84040df8675c8b1da9e4beeaf9309
Kamil Balci <kamilbalci.us@gmail.com>
2026-06-29 16:43:50 +0000

    for/elif/else loop daily practice

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # My First Repo
 This is a repository where I am practicing my terminal skils, Python, and Git pushing!
+
 <img width="736" height="414" alt="image" src="https://github.com/user-attachments/assets/c80437cc-a6d8-48f6-830f-5f6bce4b2478" />
--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
 print("Hello World")
 print("A commit can be defined as a snapshot of code changes")
 
+# Daily python practice/ challenge
+for num in range(1,21):
+	if num % 3 == 0 and num % 5 == 0:
+		print("FuzzBuzz")
+	elif num % 3 == 0:
+		print("Fuzz")
+	elif num % 5 == 0:
+		print("Buzz")
+	else:
+		print(num)
+
