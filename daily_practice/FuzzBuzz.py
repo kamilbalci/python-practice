48b78c01c5edd07190bb8d2f7dedaa72a8bd2703
Kamil Balci <kamilbalci.us@gmail.com>
2026-06-29 20:22:38 +0000

    Added user input for loop and print ranges

--- daily_practice/FuzzBuzz.py
+++ daily_practice/FuzzBuzz.py
@@ -1,25 +1,41 @@
 # Daily python practice/ challenge
 
-# Counter
+# Ask the user for the start and stop numbers 
+start_num = int(input("Enter the starting number: "))
+end_num = int(input("Enter the ending number: "))
+
+# Ask user if they want to limit  what prints to the screen
+toggle_choice = input("Do you want to limit what print? (yes/no): ").lower()
+if toggle_choice == "yes":
+    print_start = int(input("Start printing from number: "))
+    print_end = int(input("Stop printing at number: "))
+
+# Counter Room
 FuzzBuzz_count = 0
 Fuzz_count = 0
 Buzz_count = 0
 Regular_count = 0
 
-# Operation
-for num in range(1,21):
-        if num % 3 == 0 and num % 5 == 0:
-                print("FuzzBuzz")
-                FuzzBuzz_count += 1 # Add 1 to FuzzBuzz tally
-        elif num % 3 == 0:
-                print("Fuzz")
-                Fuzz_count += 1 # Add 1 to Fuzz tally
-        elif num % 5 == 0:
-                print("Buzz")
-                Buzz_count += 1 # Add 1 to Buzz tally
-        else:
-                print(num)
-                Regular_count += 1 # Add 1 to Regular tally
+# Operation Engine
+for num in range(start_num, end_num + 1):
+# Check if a specific number is allowed to print
+    should_print = (toggle_choice != "yes") or (print_start <= num <= print_end)
+    if num % 3 == 0 and num % 5 == 0:
+        if should_print:
+            print("FuzzBuzz")
+        FuzzBuzz_count += 1 # Add 1 to FuzzBuzz tally
+    elif num % 3 == 0:
+        if should_print:
+            print("Fuzz")
+        Fuzz_count += 1 # Add 1 to Fuzz tally
+    elif num % 5 == 0:
+        if should_print:
+            print("Buzz")
+        Buzz_count += 1 # Add 1 to Buzz tally
+    else:
+        if should_print:
+            print(num)
+        Regular_count += 1 # Add 1 to Regular tally
 
 # Scores
 print("--- FINAL SCORES ---")
